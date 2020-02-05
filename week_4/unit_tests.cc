4391bb0c3211cf9351912a4bbe8d51b06278c50a
Alexander Castro <Adawg@Alexanders-MacBook-Pro.local>
2020-02-04 23:27:06 -0800

    will use the complex datatype for tests

--- week_4/unit_tests.cc
+++ week_4/unit_tests.cc
@@ -2,10 +2,14 @@
 #include <float.h> /* defines DBL_EPSILON */
 #include <assert.h>
 #include "typed_array.h"
-#include "point.h"
+#include "mpoint.h"
 #include "gtest/gtest.h"
 
 namespace {
+
+//**********************************************************************//
+//           These are  tests for the TypedArray data type              // 
+//**********************************************************************//
 
     TEST(TypedArray, Construction) {
         TypedArray<Point> b;
@@ -62,6 +66,29 @@ namespace {
                                                // then we would expect m[0][0]
                                                // to be x[0], which we changed 
                                                // to -1.
+    }
+
+//**********************************************************************//
+//           These are  tests for the Complex data type                 // 
+//**********************************************************************//
+    TEST(Examples,Complex) {
+        Complex x(1,2), y(3);
+    }
+
+    template<typename T>
+    int compare(const T& x, const T& y) {
+        if ( x < y ) {
+            return -1;
+        } else if ( y < x ) {
+            return 1;
+        } else {
+            return 0;
+        }
     }    
+
+    TEST(Examples,Templates) {
+        EXPECT_EQ(compare(1.0, 2.0), -1);
+        EXPECT_EQ(compare(Complex(5,4), Complex(-3,4)), 1);
+    }
 
 }
