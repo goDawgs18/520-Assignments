f134f6421d3e68e2572d685d1921039cd32902f9
Alexander Castro <Adawg@Alexanders-MacBook-Pro.local>
2020-02-19 22:33:16 -0800

    yolo not throughly testing code bc b_day

--- week_6/unit_tests.cc
+++ week_6/unit_tests.cc
@@ -1,5 +1,9 @@
-#include "stopwatch.h"
 #include "gtest/gtest.h"
+#include "stopwatch.h"
+#include "random_process.h"
+#include "integrator.h"
+#include "derivative.h"
+
 
 namespace {
 
@@ -16,5 +20,21 @@ namespace {
         EXPECT_EQ(s.get_seconds(), .01);
         EXPECT_EQ(s.get_milliseconds(), 10);
         EXPECT_EQ(s.get_nanoseconds(), 10000000);
+    }
+
+    TEST(RandomProcess,NiceUsage) {
+        elma::Manager m;
+
+        RandomProcess r("random numbers");
+        Filter f("filter");
+        elma::Channel link("link");
+
+        m.schedule(r, 1ms)
+        .schedule(f, 1ms)
+        .add_channel(link)
+        .init()
+        .run(1000ms);
+
+        std::cout << f.value() << "\n";
     }
 }
